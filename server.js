7d866d2c1b48674cc7097e33679ba2c4259a96b7
james ekene <edehjamesraphael@gmail.com>
2022-05-02 14:48:23 +0100

    qa

--- server.js
+++ server.js
@@ -163,6 +163,14 @@ app.use("/api/chatStructure", chatStructure);
 app.use("/api/chats", chats);
  
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 const server = http.listen(
