9ed0567b8d2e51b6a976579b185e17329c9715ae
Jame Ekene <edehjamesraphael@gmail.com>
2023-01-03 11:04:39 +0100

    static files 2

--- server/src/server.js
+++ server/src/server.js
@@ -168,9 +168,15 @@ app.get("/hello", (_, res) => res.send("Hello from chat app"));
 app.use("/api/auth", auth);
 app.use("/api/chatStructure", chatStructure);
 app.use("/api/chats", chats);
-app.use("/file", express.static(path.join(__dirname, "/public", "/uploads")));
+// app.use("/file", express.static(path.join(__dirname, "/public", "/uploads")));
 
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  app.get("/file", (_, res) =>
+    res.sendFile(path.resolve(__dirname, "/public", "/uploads"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
